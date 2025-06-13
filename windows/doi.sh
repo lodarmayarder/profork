e3b4d73608e1d127d74e3f20c01cacdc144e3d63
Cliffy <cliffypro@proton.me>
2025-06-13 13:06:58 +0300

    Update doi.sh

--- windows/doi.sh
+++ windows/doi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define your variables for easy customization
-URL="https://github.com/DTJW92/batocera-unofficial-addons/releases/download/AppImages/Zelda-DungeonsOfInfinity.wsquashfs"
+URL="https://github.com/profork/profork/releases/download/r1/Zelda-DungeonsOfInfinity.wsquashfs"
 KEYS_URL=""  # Leave empty if no keys file is needed
 DEST_DIR="/userdata/roms/windows"
 MESSAGE=""  # Leave empty if no message is needed
