b23444e48a3324f750d773f9422d26480437a890
Cliffy <cliffypro@proton.me>
2025-08-27 16:30:04 +0300

    +

--- portmaster/portmaster_x64.sh
+++ portmaster/portmaster_x64.sh
@@ -4,7 +4,7 @@ dialog --msgbox "Portmaster will be installed to ports. It will usually launch o
 cd /userdata/system
 rm -f Install.Full.PortMaster.sh
 wget https://github.com/PortsMaster/PortMaster-GUI/releases/download/2024.12.16-1112/Install.PortMaster.sh
-chmod +x Install.Full.PortMaster.sh
+chmod +x Install.PortMaster.sh
 (./Install.Full.PortMaster.sh)
 rm Install.Full.PortMaster.sh
 dialog --msgbox "Portmaster is installed to ports. Refresh/update the gamelist. It will usually launch on the second launch attempt. You may need to force close it if it hangs on the portmaster logo on first launch.\n\nGame compatibility varies." 10 60
