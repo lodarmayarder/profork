e200e030e24e22b30a710c4d0960f95e56c31b37
Cliffy <cliffypro@proton.me>
2025-09-02 11:00:25 +0300

    +

--- app/fpg.sh
+++ app/fpg.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # === BUA Detection ===
-if [ -d "/userdata/system/add-ons" ]; then
-    rm -f /userdata/roms/ports/Profork.sh
-    rm -r /userdata/roms/ports/Profork.sh.keys
-    clear
-    echo "BUA detected."
-    echo "Dual installs not recommended"
-    sleep 5
-fi
+#if [ -d "/userdata/system/add-ons" ]; then
+#    rm -f /userdata/roms/ports/Profork.sh
+ #   rm -r /userdata/roms/ports/Profork.sh.keys
+ #   clear
+ #   echo "BUA detected."
+ #   echo "Dual installs not recommended"
+ #   sleep 5
+# fi
 # This script uses DIALOG to display an alphabetically sorted checklist of Flatpak games.
 # Each entry includes a description. When you select games and confirm, the script installs
 # them system‑wide (using --system). Run the script as root or via sudo.
